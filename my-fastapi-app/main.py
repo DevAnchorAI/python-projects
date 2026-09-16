4e5ff57b362543cd7635508a7fa96e16e8f65958
sumankumarcs92 <109429136+sumankumarcs92@users.noreply.github.com>
2026-09-17 00:05:45 +0530

    loggger added

--- my-fastapi-app/main.py
+++ my-fastapi-app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+import logging;
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 @app.get("/")
 def home():
+    logging.info("Inside Defalt API")
     return {"message": "Hello World"}
 
-@app.get("/hello/{name}")
+@app.get("/greeting/{name}")
 def greeting(name:str):
+    logging.info("Inside greeting API")
     return "HELLO:"+name;
