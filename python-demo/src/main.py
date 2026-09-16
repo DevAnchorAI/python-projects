9969f5d584c4d8f5d6f5dcc0310f070d165c5866
sumankumarcs92 <109429136+sumankumarcs92@users.noreply.github.com>
2026-09-17 00:01:05 +0530

    python basic practice

--- python-demo/src/main.py
+++ python-demo/src/main.py
@@ -1,5 +1,6 @@
 
 import utils as ut;
+from employee import Employee;
 
 if __name__ == "__main__":
     name="Alice";
@@ -10,3 +11,37 @@ if __name__ == "__main__":
     print(f"Subtraction: {ut.subtract(a, b)}");
     print(f"Multiplication: {ut.multiply(a, b)}");
     print(f"Division: {ut.divide(a, b)}");
+    print(f"Hello Python");
+    
+    employee = {
+    "id": 101,
+    "name": "John",
+    "salary": 75000
+}
+
+print("salary:",employee["salary"])
+if employee["salary"]>80000:
+   print(f"Good Salary");
+elif employee["salary"]>50000:
+    print(f"Average salary");  
+else:
+    print(f"Bad salary");
+
+
+# for i in range(5):
+#     print(i)
+
+employees = ["John", "David", "Robert"]
+for employee in employees:
+    print(employee)
+
+#Function
+def add(a,b):
+    return a+b;
+
+result = add(5,6);
+print(result)
+
+employee= Employee("SUMAN");
+print(employee.name)
+
